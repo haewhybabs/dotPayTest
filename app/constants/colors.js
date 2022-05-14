f5ac2e61cfd6946e2a9c2ceee23b183964748f90
Ayobami Babalola <babalolaisaac@gmail.com>
2022-05-14 14:49:00 +0100

    basic text components implemented

--- app/constants/colors.js
+++ app/constants/colors.js
@@ -1,1 +1,7 @@
 export const splashColor = '#E4FFE0';
+export const textColor = "#121212";
+export const pageBackground = "=#ffffff";
+export const categoryHeader = "=#bac8d3";
+export const nomineeCard = "=#dae8fc";
+export const selectedNomineeCard = "#d5e8d4";
+export const buttonBackground = "#f5f5f5"
